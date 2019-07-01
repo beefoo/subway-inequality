9f1107320405116e057b95d6569685d4e54f1d50
beefoo <brian@youaremyjoy.org>
2019-06-30 22:49:36 -0400

    adjust volume logic, add bullets, scaffold frame draw

--- lib.py
+++ lib.py
@@ -1,8 +1,10 @@
 import csv
+import glob
 import math
 import numpy as np
 import os
 from pprint import pprint
+from pydub import AudioSegment
 import random
 import sys
 import time
@@ -11,6 +13,19 @@ def addIndices(arr, keyName="index", startIndex=0):
     for i, item in enumerate(arr):
         arr[i][keyName] = startIndex + i
     return arr
+
+def addNormalizedValues(arr, key, nkey):
+    values = [v[key] for v in arr]
+    range = (min(values), max(values))
+    for i, entry in enumerate(arr):
+        arr[i][nkey] = norm(entry[key], range)
+    return arr
+
+def ceilInt(n):
+    return int(math.ceil(n))
+
+def ceilToNearest(n, nearest):
+    return 1.0 * math.ceil(1.0*n/nearest) * nearest
 
 def createLookup(arr, key):
     return dict([(str(item[key]), item) for item in arr])
@@ -43,6 +58,35 @@ def formatSeconds(s):
         tString = tString[3:]
     return tString
 
+def frameToMs(frame, fps, roundResult=True):
+    result = (1.0 * frame / fps) * 1000.0
+    if roundResult:
+        result = roundInt(result)
+    return result
+
+# Note: sample_width -> bit_depth conversions: 1->8, 2->16, 3->24, 4->32
+# 24/32 bit depth and 48K sample rates are industry standards
+def getAudio(filename, sampleWidth=4, sampleRate=48000, channels=2):
+    # A hack: always read files at 16-bit depth because Sox does not support more than that
+    fformat = filename.split(".")[-1].lower()
+    audio = AudioSegment.from_file(filename, format=fformat)
+    # convert to stereo
+    if audio.channels != channels:
+        print("Warning: channels changed to %s from %s in %s" % (channels, audio.channels, filename))
+        audio = audio.set_channels(channels)
+    # convert sample width
+    if audio.sample_width != sampleWidth:
+        print("Warning: sample width changed to %s from %s in %s" % (sampleWidth, audio.sample_width, filename))
+        audio = audio.set_sample_width(sampleWidth)
+    # convert sample rate
+    if audio.frame_rate != sampleRate:
+        print("Warning: frame rate changed to %s from %s in %s" % (sampleRate, audio.frame_rate, filename))
+        audio = audio.set_frame_rate(sampleRate)
+    return audio
+
+def getBasename(fn):
+    return os.path.splitext(os.path.basename(fn))[0]
+
 # https://codereview.stackexchange.com/questions/28207/finding-the-closest-point-to-a-list-of-points
 def getSortedIndicesByDistance(node, nodes):
     deltas = nodes - node
@@ -63,6 +107,89 @@ def lerp(ab, amount):
 def lim(value, ab=(0, 1)):
     a, b = ab
     return max(a, min(b, value))
+
+def makeDirectories(filenames):
+    if not isinstance(filenames, list):
+        filenames = [filenames]
+    for filename in filenames:
+        dirname = os.path.dirname(filename)
+        if len(dirname) > 0 and not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+def makeTrack(duration, instructions, audio, sampleWidth=4, sampleRate=48000, channels=2):
+    # build audio
+    baseAudio = AudioSegment.silent(duration=duration, frame_rate=sampleRate)
+    baseAudio = baseAudio.set_channels(channels)
+    baseAudio = baseAudio.set_sample_width(sampleWidth)
+    instructionCount = len(instructions)
+
+    # convert sample width
+    if audio.sample_width != sampleWidth:
+        # print("Warning: sample width changed to %s from %s" % (sampleWidth, audio.sample_width))
+        audio = audio.set_sample_width(sampleWidth)
+    # add slight fade to avoid clicking
+    audio = audio.fade_in(10).fade_out(10)
+
+    for index, i in enumerate(instructions):
+        stepAudio = audio.apply_gain(i["gain"])
+        baseAudio = baseAudio.overlay(stepAudio, position=i["ms"])
+        printProgress(index+1, instructionCount)
+    return baseAudio
+
+def mixAudio(instructions, duration, outfilename, sampleWidth=4, sampleRate=48000, channels=2, masterDb=0.0):
+    # remove instructions with no volume
+    instructions = [i for i in instructions if "volume" not in i or i["volume"] > 0]
+    audioFiles = list(set([i["filename"] for i in instructions]))
+    audioFiles = [{"filename": f} for f in audioFiles]
+    instructionCount = len(instructions)
+    trackCount = len(audioFiles)
+
+    # calculate db
+    for i, step in enumerate(instructions):
+        if "volume" in step:
+            instructions[i]["gain"] = volumeToDb(step["volume"])
+
+    # create base audio
+    baseAudio = AudioSegment.silent(duration=duration, frame_rate=sampleRate)
+    baseAudio = baseAudio.set_channels(channels)
+    baseAudio = baseAudio.set_sample_width(sampleWidth)
+
+    # Load sounds
+    print("Adding tracks...")
+    for i, af in enumerate(audioFiles):
+        filename = af["filename"]
+        audioFiles[i]["index"] = i
+
+        # load audio file
+        audio = getAudio(filename, sampleWidth, sampleRate, channels)
+        audioDurationMs = len(audio)
+
+        # make the track
+        trackInstructions = [ii for ii in instructions if ii["filename"]==af["filename"]]
+        print("Making track %s of %s with %s instructions..." % (i+1, trackCount, len(trackInstructions)))
+        trackAudio = makeTrack(duration, trackInstructions, audio, sampleWidth=sampleWidth, sampleRate=sampleRate, channels=channels)
+        baseAudio = baseAudio.overlay(trackAudio)
+        print("Track %s of %s complete." % (i+1, trackCount))
+
+    print("Writing to file...")
+    format = outfilename.split(".")[-1]
+    # adjust master volume
+    if masterDb != 0.0:
+        baseAudio = baseAudio.apply_gain(masterDb)
+    f = baseAudio.export(outfilename, format=format)
+    print("Wrote to %s" % outfilename)
+
+def msToFrame(ms, fps):
+    return roundInt((ms / 1000.0) * fps)
+
+def norm(value, ab, limit=False):
+    a, b = ab
+    n = 0.0
+    if (b - a) != 0:
+        n = 1.0 * (value - a) / (b - a)
+    if limit:
+        n = lim(n)
+    return n
 
 def parseNumber(string, alwaysFloat=False):
     try:
@@ -135,11 +262,28 @@ def readCsv(filename, doParseNumbers=True, skipLines=0, encoding="utf8", readDic
             print("Read %s rows from %s" % (len(rows), filename))
     return (fieldnames, rows)
 
+def removeFiles(listOrString):
+    filenames = listOrString
+    if not isinstance(listOrString, list) and "*" in listOrString:
+        filenames = glob.glob(listOrString)
+    elif not isinstance(listOrString, list):
+        filenames = [listOrString]
+    print("Removing %s files" % len(filenames))
+    for fn in filenames:
+        if os.path.isfile(fn):
+            os.remove(fn)
+
 def roundInt(val):
     return int(round(val))
 
 def roundToNearest(n, nearest):
     return 1.0 * round(1.0*n/nearest) * nearest
+
+def volumeToDb(volume):
+    db = 0.0
+    if 0.0 < volume < 1.0 or volume > 1.0:
+        db = 10.0 * math.log(volume**2)
+    return db
 
 def weightedMean(values, weights):
     return np.average(values, weights=weights)
@@ -155,3 +299,7 @@ def writeCsv(filename, arr, headings):
                 row.append(value)
             writer.writerow(row)
     print("Wrote %s rows to %s" % (len(arr), filename))
+
+def zeroPad(value, total):
+    padding = len(str(total))
+    return str(value).zfill(padding)
--- make.py
+++ make.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+# import gizeh
 import os
+from PIL import Image, ImageDraw
 from pprint import pprint
 import sys
 
@@ -10,17 +12,23 @@ from lib import *
 # input
 parser = argparse.ArgumentParser()
 parser.add_argument('-data', dest="DATA_FILE", default="data/lines/2.csv", help="Input csv file with preprocessed data")
+parser.add_argument('-img', dest="IMAGE_FILE", default="img/2.png", help="Subway bullet image")
 parser.add_argument('-instruments', dest="INSTRUMENTS_FILE", default="data/instruments.csv", help="Input csv file with instruments config")
 parser.add_argument('-dir', dest="MEDIA_DIRECTORY", default="audio/", help="Input media directory")
 parser.add_argument('-width', dest="WIDTH", default=1920, type=int, help="Output video width")
 parser.add_argument('-height', dest="HEIGHT", default=1080, type=int, help="Output video height")
+parser.add_argument('-pad0', dest="PAD_START", default=2000, type=int, help="Pad start in ms")
+parser.add_argument('-pad1', dest="PAD_END", default=4000, type=int, help="Pad end in ms")
 parser.add_argument('-fps', dest="FPS", default=60, type=int, help="Output video frames per second")
 parser.add_argument('-outframe', dest="OUTPUT_FRAME", default="tmp/line_%s/frame.%s.png", help="Output frames pattern")
+parser.add_argument('-aout', dest="AUDIO_OUTPUT_FILE", default="output/subway_line_%s.mp3", help="Output audio file")
 parser.add_argument('-out', dest="OUTPUT_FILE", default="output/subway_line_%s.mp4", help="Output media file")
+parser.add_argument('-overwrite', dest="OVERWRITE", action="store_true", help="Overwrite existing files?")
 parser.add_argument('-probe', dest="PROBE", action="store_true", help="Just view statistics?")
 parser.add_argument('-reverse', dest="REVERSE", action="store_true", help="Reverse the line?")
 
 # Music config
+parser.add_argument('-gain', dest="GAIN_DB", type=float, default=-4.0, help="Gain to apply to each clip in decibels")
 parser.add_argument('-db', dest="MASTER_DB", type=float, default=0.0, help="Master +/- decibels to be applied to final audio")
 parser.add_argument('-bpm', dest="BPM", type=int, default=120, help="Beats per minute, e.g. 60, 75, 100, 120, 150")
 parser.add_argument('-mpb', dest="METERS_PER_BEAT", type=int, default=75, help="Higher numbers creates shorter songs")
@@ -29,7 +37,24 @@ parser.add_argument('-pm', dest="PRICE_MULTIPLIER", type=float, default=1.14, he
 parser.add_argument('-vdur', dest="VARIANCE_MS", type=int, default=20, help="+/- milliseconds an instrument note should be off by to give it a little more 'natural' feel")
 
 # Visual design config
-
+parser.add_argument('-sw', dest="STATION_WIDTH", type=float, default=0.3125, help="Minumum station width as a percent of the screen width; adjust this to change the overall visual speed")
+parser.add_argument('-tw', dest="TEXT_WIDTH", type=float, default=0.5, help="Station text width as a percent of the screen width")
+parser.add_argument('-cy', dest="CENTER_Y", type=float, default=0.5, help="Center y as a percent of screen height")
+parser.add_argument('-bty', dest="BOROUGH_TEXT_Y", type=float, default=0.375, help="Borough text center y as a percent of screen height")
+parser.add_argument('-sty', dest="STATION_TEXT_Y", type=float, default=0.625, help="Station text center y as a percent of screen height")
+parser.add_argument('-cw', dest="CIRCLE_WIDTH", type=int, default=90, help="Circle radius in pixels assuming 1920x1080")
+parser.add_argument('-lh', dest="LINE_HEIGHT", type=int, default=28, help="Height of horizontal line in pixels assuming 1920x1080")
+parser.add_argument('-bh', dest="BOUNDARY_HEIGHT", type=int, default=240, help="Height of horizontal line in pixels assuming 1920x1080")
+parser.add_argument('-bw', dest="BOUNDARY_WIDTH", type=int, default=3, help="Height of horizontal line in pixels assuming 1920x1080")
+parser.add_argument('-mw', dest="MARKER_WIDTH", type=int, default=8, help="Height of horizontal line in pixels assuming 1920x1080")
+parser.add_argument('-sts', dest="STATION_TEXT_SIZE", type=int, default=60, help="Station text size in pixels assuming 1920x1080")
+parser.add_argument('-bts', dest="BOROUGH_TEXT_SIZE", type=int, default=48, help="Borough text size in pixels assuming 1920x1080")
+parser.add_argument('-bg', dest="BG_COLOR", default="#000000", help="Background color")
+parser.add_argument('-tc', dest="TEXT_COLOR", default="#eeeeee", help="Text color")
+parser.add_argument('-atc', dest="ALT_TEXT_COLOR", default="#aaaaaa", help="Secondary text color")
+parser.add_argument('-mc', dest="MARKER_COLOR", default="#dddddd", help="Marker color")
+parser.add_argument('-sfont', dest="STATION_FONT", default="fonts/OpenSans-Bold.ttf", help="Station font")
+parser.add_argument('-bfont', dest="BOROUGH_FONT", default="fonts/OpenSans-SemiBold.ttf", help="Borough font")
 a = parser.parse_args()
 
 # Calculations
@@ -70,16 +95,18 @@ def buyInstruments(station, instrumentsShelf):
 
 # Parse stations
 stations = sorted(stations, key=lambda d: d["income"])
+stations = addNormalizedValues(stations, "income", "nIncome")
 stations = addIndices(stations, "incomeIndex")
 stations = sorted(stations, key=lambda d: d["sortBy"], reverse=a.REVERSE)
 stations = addIndices(stations, "index")
 stationCount = len(stations)
+ms = a.PAD_START
 for i, station in enumerate(stations):
     stations[i]["percentile"] = 1.0 * station["incomeIndex"] / stationCount * 100
+    # stations[i]["percentile"] = min(99.999, 1.0 * station["nIncome"] * 100)
     stations[i]["instruments"] = buyInstruments(stations[i], instruments)
     # print(len(stations[i]["instruments"]))
     distance = beats = duration = 0
-    boroughNext = station["Borough"]
     if i < stationCount-1:
         distance = earthDistance(stations[i+1]['GTFS Latitude'], stations[i+1]['GTFS Longitude'], station['GTFS Latitude'], station['GTFS Longitude'])
         beats = roundInt(1.0 * distance / a.METERS_PER_BEAT)
@@ -89,6 +116,8 @@ for i, station in enumerate(stations):
     stations[i]["beats"] = beats
     stations[i]["duration"] = duration
     stations[i]["BoroughNext"] = boroughNext
+    stations[i]["ms"] = ms
+    ms += duration
 
 # Calculate ranges
 distances = [s["distance"] for s in stations if s["distance"] > 0]
@@ -106,19 +135,16 @@ print('Distance range in meters: [%s, %s]' % (roundInt(minDistance), roundInt(ma
 print('Average beats per station: %s' % roundInt(1.0*totalBeats/stationCount))
 print('Average time per station: %s' % formatSeconds(secondsPerStation))
 print('Main sequence beats: %s' % totalBeats)
-print('Main sequence time: %s' % formatSeconds(totalSeconds))
 
 # Retrieve gain based on current beat
-def getGain(instrument, beat):
+def getVolume(instrument, beat):
     beats_per_phase = instrument['gain_phase']
     percent_complete = float(beat % beats_per_phase) / beats_per_phase
-    multiplier = ease(percent_complete)
-    from_gain = instrument['from_gain']
-    to_gain = instrument['to_gain']
-    min_gain = min(from_gain, to_gain)
-    gain = multiplier * (to_gain - from_gain) + from_gain
-    gain = max(min_gain, round(gain, 2))
-    return gain
+    percent = ease(percent_complete)
+    from_volume = instrument['from_volume']
+    to_volume = instrument['to_volume']
+    volume = lerp((from_volume, to_volume), percent)
+    return volume
 
 # Get beat duration in ms based on current point in time
 def getBeatMs(instrument, beat, round_to):
@@ -126,8 +152,8 @@ def getBeatMs(instrument, beat, round_to):
     to_beat_ms = instrument['to_beat_ms']
     beats_per_phase = instrument['tempo_phase']
     percent_complete = float(beat % beats_per_phase) / beats_per_phase
-    multiplier = ease(percent_complete)
-    ms = multiplier * (to_beat_ms - from_beat_ms) + from_beat_ms
+    percent = ease(percent_complete)
+    ms = lerp((from_beat_ms, to_beat_ms), percent)
     ms = roundInt(roundToNearest(ms, round_to))
     return ms
 
@@ -139,7 +165,7 @@ def isValidInterval(instrument, elapsed_ms):
     return int(math.floor(1.0*elapsed_ms/interval_ms)) % interval == interval_offset
 
 # Add beats to sequence
-def addBeatsToSequence(sequence, instrument, duration, ms, beat_ms, round_to):
+def addBeatsToSequence(sequence, instrument, duration, ms, beat_ms, round_to, pad_start):
     offset_ms = int(instrument['tempo_offset'] * beat_ms)
     ms += offset_ms
     previous_ms = int(ms)
@@ -162,8 +188,9 @@ def addBeatsToSequence(sequence, instrument, duration, ms, beat_ms, round_to):
             variance = roundInt(rn * a.VARIANCE_MS * 2 - a.VARIANCE_MS)
             sequence.append({
                 'filename': instrument["file"],
-                'gain': getGain(instrument, elapsed_beat),
-                'ms': max([elapsed_ms + variance, 0])
+                # 'gain': getGain(instrument, elapsed_beat) + a.GAIN_DB,
+                'volume': getVolume(instrument, elapsed_beat),
+                'ms': max([pad_start + elapsed_ms + variance, 0])
             })
         remaining_duration -= this_beat_ms
         elapsed_duration += this_beat_ms
@@ -181,7 +208,7 @@ for i, instrument in enumerate(instruments):
         instrumentIndex = findInList(station['instruments'], 'index', instrument['index'])
         # Instrument not here, just add the station duration and continue
         if instrumentIndex < 0 and stationQueueDur > 0:
-            sequence = addBeatsToSequence(sequence, instrument, stationQueueDur, ms, BEAT_MS, ROUND_TO_NEAREST)
+            sequence = addBeatsToSequence(sequence, instrument, stationQueueDur, ms, BEAT_MS, ROUND_TO_NEAREST, a.PAD_START)
             ms += stationQueueDur + station['duration']
             stationQueueDur = 0
         elif instrumentIndex < 0:
@@ -189,6 +216,130 @@ for i, instrument in enumerate(instruments):
         else:
             stationQueueDur += station['duration']
     if stationQueueDur > 0:
-        sequence = addBeatsToSequence(sequence, instrument, stationQueueDur, ms, BEAT_MS, ROUND_TO_NEAREST)
+        sequence = addBeatsToSequence(sequence, instrument, stationQueueDur, ms, BEAT_MS, ROUND_TO_NEAREST, a.PAD_START)
+sequenceDuration = max([s["ms"] for s in sequence]) + a.PAD_END
 
+# Now start the video frame logic
+
+# Calculations
+aa = vars(a)
+aa["STATION_WIDTH"] = roundInt(1.0 * a.WIDTH * a.STATION_WIDTH)
+aa["TEXT_WIDTH"] = roundInt(1.0 * a.WIDTH * a.TEXT_WIDTH)
+aa["CENTER_Y"] = roundInt(1.0 * a.HEIGHT * a.CENTER_Y)
+aa["BOROUGH_TEXT_Y"] = roundInt(1.0 * a.HEIGHT * a.BOROUGH_TEXT_Y)
+aa["STATION_TEXT_Y"] = roundInt(1.0 * a.HEIGHT * a.STATION_TEXT_Y)
+RESOLUTION = a.WIDTH / 1920.0
+aa["CIRCLE_WIDTH"] = roundInt(a.CIRCLE_WIDTH * RESOLUTION)
+aa["LINE_HEIGHT"] = roundInt(a.LINE_HEIGHT * RESOLUTION)
+aa["BOUNDARY_HEIGHT"] = roundInt(a.BOUNDARY_HEIGHT * RESOLUTION)
+aa["BOUNDARY_WIDTH"] = roundInt(a.BOUNDARY_WIDTH * RESOLUTION)
+aa["MARKER_WIDTH"] = roundInt(a.MARKER_WIDTH * RESOLUTION)
+aa["STATION_TEXT_SIZE"] = roundInt(a.STATION_TEXT_SIZE * RESOLUTION)
+aa["BOROUGH_TEXT_SIZE"] = roundInt(a.BOROUGH_TEXT_SIZE * RESOLUTION)
+
+x = 0
+for i, station in enumerate(stations):
+    boroughNext = station["Borough"]
+    if i < stationCount-1:
+        boroughNext = stations[i+1]["Borough"]
+    stations[i]["BoroughNext"] = boroughNext
+    stations[i]["width"] = roundInt(1.0 * station["duration"] / minDuration * a.STATION_WIDTH)
+    stations[i]["x"] = x
+    stations[i]["x0"] = x - a.TEXT_WIDTH / 2
+    stations[i]["x1"] = x + a.TEXT_WIDTH / 2
+    x += stations[i]["width"]
+totalW = x
+pxPerMs = 1.0 * totalW / totalMs
+pxPerS = pxPerMs * 1000.0
+pxPerFrame = pxPerS / a.FPS
+print("Total width: %s px" % totalW)
+print("Pixels per second: %s" % pxPerS)
+print("Pixels per frame: %s" % pxPerFrame)
+
+totalFrames = msToFrame(sequenceDuration, a.FPS)
+totalFrames = int(ceilToNearest(totalFrames, a.FPS))
+sequenceDuration = frameToMs(totalFrames, a.FPS)
+basename = getBasename(a.DATA_FILE)
+
+def drawFrame(filename, xOffset, stations, totalW, bulletImg, a):
+    if not a.OVERWRITE and os.path.isfile(filename):
+        return
+
+    im = Image.new('RGB', (a.WIDTH, a.HEIGHT), a.BG_COLOR)
+    draw = ImageDraw.Draw(im, 'RGBA')
+    cx = roundInt(a.WIDTH * 0.5)
+    cy = roundInt(a.HEIGHT * 0.5)
+
+    leftX = xOffset
+    rightX = leftX + totalW
+
+    # draw the center line
+    x0 = 0 if leftX < 0 else leftX
+    x1 = a.WIDTH if rightX > a.WIDTH else rightX
+    y0 = cy - a.LINE_HEIGHT/2
+    y1 = y0 + a.LINE_HEIGHT
+    draw.rectangle([(x0, y0), (x1, y1)], fill=a.ALT_TEXT_COLOR)
+
+    for i, s in enumerate(stations):
+        # check if station is visible
+        sx0 = xOffset + s["x0"]
+        sx1 = xOffset + s["x1"]
+        if not (0 <= sx0 <= a.WIDTH or 0 <= sx1 <= a.WIDTH):
+            continue
+
+        sx = xOffset + s["x"]
+        sy = a.CENTER_Y
+
+        # draw borough text
+
+        # draw bullet
+        bx = roundInt(sx - a.CIRCLE_WIDTH/2)
+        by = roundInt(sy - a.CIRCLE_WIDTH/2)
+        im.paste(bulletImg, (bx, by), bulletImg)
+
+        # draw station text
+
+    # draw the marker
+    x0 = cx - a.MARKER_WIDTH/2
+    x1 = x0 + a.MARKER_WIDTH
+    y0 = 0
+    y1 = a.HEIGHT
+    draw.rectangle([(x0, y0), (x1, y1)], fill=(255,255,255,100))
+
+    del draw
+    im.save(filename)
+    print("Saved %s" % filename)
+
+bulletImg = Image.open(a.IMAGE_FILE)
+bulletImg = bulletImg.resize((a.CIRCLE_WIDTH, a.CIRCLE_WIDTH), resample=Image.LANCZOS)
+
+audioFilename = a.AUDIO_OUTPUT_FILE % basename
 print("%s steps in sequence" % len(sequence))
+print('Total sequence time: %s' % formatSeconds(sequenceDuration/1000.0))
+
+if a.PROBE:
+    sys.exit()
+
+makeDirectories([a.OUTPUT_FRAME % (basename, "*")])
+
+if a.OVERWRITE:
+    removeFiles(a.OUTPUT_FRAME % (basename, "*"))
+
+print("Making video frame sequence...")
+videoFrames = []
+xOffset = roundInt(a.WIDTH * 0.5)
+for f in range(totalFrames):
+    frame = f + 1
+    ms = frameToMs(frame, a.FPS)
+    frameFilename = a.OUTPUT_FRAME % (basename, zeroPad(frame, totalFrames))
+    drawFrame(frameFilename, xOffset, stations, totalW, bulletImg, a)
+
+    if a.PAD_START <= ms < a.PAD_END:
+        xOffset -= pxPerFrame
+
+    break
+
+if a.OVERWRITE or not os.path.isfile(audioFilename):
+    mixAudio(sequence, sequenceDuration, audioFilename, masterDb=a.MASTER_DB)
+else:
+    print("%s already exists" % audioFilename)
